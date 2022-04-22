c35d4b3dc2e3ace946765e0067752f9bbde1c3b5
RehamMuhammad <reham.muhammad.helmy@gmail.com>
2022-04-23 00:15:38 +0200

    Initial Alert Component Structure

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,8 +1,23 @@
-export default function Alert() {
-    return (
-        <div>
-          <span> &times;</span>
-            message
-        </div>
-      );
-    }
+import style from "./style.module.css";
+import css from "classnames";
+import React from "react";
+
+export default function Alert({ children, type, message }) {
+
+  /* The React.cloneElement() function returns a copy of a specified element.
+  Additional props and children can be passed on in the function.
+  You would use this function when a parent component wants to add or modify the prop(s) of its children.*/
+
+  const renderElAlert = function () {
+    return React.cloneElement(children);
+  };
+
+  return (
+    <div className={css(style.alert, style[type])}>
+      <span className={style.closebtn}>
+        &times;
+      </span>
+      {children ? renderElAlert() : message}
+    </div>
+  );
+}
